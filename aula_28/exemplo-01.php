6ffc910eadc889eb6e15576683d0226f1e78e856
Gabriel Klein <gabrielkleinribas@gmail.com>
2019-08-02 16:56:01 -0300

    aula 28

---
+++ aula_28/exemplo-01.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pessoa {
+
+	public $nome; //Atributo
+
+	public function falar() { //Método
+
+		return "O meu nome é ".$this->nome;
+
+	}
+}
+
+$gabriel = new Pessoa();
+$gabriel->nome = "Gabriel";
+echo $gabriel->falar();
+
+?>
